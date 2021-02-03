cb60b2e383603a1e9753d09b949839e93bca1e5f
shaarangg <shaaranggsingh@gmail.com>
2021-02-03 09:38:09 +0530

    feat: Did kefa question

--- codeforces/kefa.py
+++ codeforces/kefa.py
@@ -1,13 +1,14 @@
 n = int(input())
 a = list(map(int,input().split()))
 max=0
+l=1
 for i in range(n-1):
-    l=1
-    for j in range(i,n-1):
-        if(a[j]<=a[j+1]):
-            l+=1
-        else:
-            break
-    if(max<l):
-        max=l
+    if(a[i]>a[i+1]):
+        if(max<l):
+            max=l
+        l=1
+    else:
+        l+=1
+if(max<l):
+    max=l   
 print(max)
