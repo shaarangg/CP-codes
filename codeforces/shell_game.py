0e561b053f7e943064d28cd13031a07f8f1a914c
shaarangg <shaaranggsingh@gmail.com>
2021-05-28 18:47:51 +0530

    fix: Finished the shell_

--- codeforces/shell_game.py
+++ codeforces/shell_game.py
@@ -1,5 +1,10 @@
 n = int(input())
 x = int(input())
-s = ['l','m','r']
-for i in range(n):
-    if(i%2==0)
+s = [0,1,2]
+n=n%6
+for i in range(1,n+1):
+    if(i%2==0):
+        s[1],s[2]=s[2],s[1]
+    else:
+        s[0],s[1]=s[1],s[0]
+print(s[x])
