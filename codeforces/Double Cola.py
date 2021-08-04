1029e1080e69e4002123256d7a505c21d4b06f29
shaarangg <shaaranggsingh@gmail.com>
2021-08-04 10:55:17 +0530

    feat: Did Double Cola

--- codeforces/Double Cola.py
+++ codeforces/Double Cola.py
@@ -3,12 +3,16 @@ from math import log2, floor
 n = int(input())
 name = ["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"]
 t = floor(log2((n / 5) + 1))
-print(t)
 if t == 0:
     print(name[n - 1])
 else:
     n1 = 5 * ((2 ** t) - 1)
     n -= n1
-    print(n)
-    n //= 2 ** t
-    print(name[n-1])
+    if n == 0:
+        print("Howard")
+    else:
+        i = 0
+        while n > 0:
+            n -= 2 ** t
+            i += 1
+        print(name[i - 1])
