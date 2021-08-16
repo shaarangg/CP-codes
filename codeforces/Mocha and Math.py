e511b7e7b723a17591019d0c58a47a9e9b379acd
shaarangg <shaaranggsingh@gmail.com>
2021-08-16 23:15:16 +0530

    feat: Mocha and math

--- codeforces/Mocha and Math.py
+++ codeforces/Mocha and Math.py
@@ -1,20 +1,7 @@
 for t in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
-    if n == 1:
-        print(a[0])
-    else:
-        mx = 0
-        index = 0
-        m = 10 ** 9
-        for i in range(n):
-            if a[i] > mx:
-                mx = a[i]
-                index = i
-        for i in range(n):
-            if i == index:
-                continue
-            temp = a[i] & mx
-            if temp < m:
-                m = temp
-        print(m)
+    m = a[0]
+    for i in range(1, n):
+        m &= a[i]
+    print(m)
