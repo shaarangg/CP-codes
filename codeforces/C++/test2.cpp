9010641eef6d6f1142ad335c69024e8dd058d013
shaarangg <shaaranggsingh@gmail.com>
2021-09-03 16:45:04 +0530

    feat: dir structure

---
+++ codeforces/C++/test2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, t;
+    cin >> n >> t;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    return 0;
+}
